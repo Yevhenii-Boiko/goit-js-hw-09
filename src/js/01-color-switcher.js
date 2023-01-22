2a34c62a81979d0e0c0c06d27803e60adafae352
Yevhenii <boiko.yevgenii@gmail.com>
2023-01-22 15:32:14 +0200

    adds task-01 solution

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+let timerId = null;
+
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
+
+refs.startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  refs.startBtn.disabled = true;
+});
+
+refs.stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  refs.startBtn.disabled = false;
+});
